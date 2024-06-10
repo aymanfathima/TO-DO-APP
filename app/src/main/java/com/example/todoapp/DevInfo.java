96a936877ee06108b3f5072094634f6d03338699
aymanfathima12 <aymnfathm12@gmail.com>
2024-06-10 16:31:19 +0530

    first commit

---
+++ app/src/main/java/com/example/todoapp/DevInfo.java
@@ -0,0 +1,14 @@
+package com.example.todoapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DevInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_info);
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/todoData.java
@@ -0,0 +1,14 @@
+package com.example.todoapp;
+
+public class todoData {
+    String todoItem;
+    int edit,delete;
+
+
+    public todoData(String todoItem,int edit,int delete){
+        this.todoItem = todoItem;
+        this.delete = delete;
+        this.edit = edit;
+
+    }
+}
